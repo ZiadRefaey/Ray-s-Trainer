cacdc183d25a581ea2bf3490a82806f1a55beae3
ZiadRefaey <ziad.refaey1@gmail.com>
2023-02-28 01:53:50 +0200

    Title Typing Feature

--- scripts/script.js
+++ scripts/script.js
@@ -3,6 +3,7 @@ const hamburger = document.querySelector(".hamburger");
 const mobileNav = document.querySelector(".mobile-nav");
 const body = document.querySelector(".body");
 const xMark = document.querySelector(".fa-xmark");
+const title = document.querySelector('.title__span');
 document.querySelector(".hamburger").addEventListener("click", function () {
   mobileNav.classList.add("is-active");
   body.classList.add("mobile-nav__active");
@@ -27,3 +28,46 @@ window.addEventListener("scroll", function () {
   }
   prevScrollPos = currentScrollPos;
 });
+// Title  Typing with cursor
+
+        const textEl = document.querySelector(".title__span");
+        const cursorEl = document.querySelector(".cursor");
+        const phrases = [  "Mark Williams","a Personal Trainer"];
+        let phraseIndex = 0;
+        let letterIndex = 0;
+      
+        function deleteText() {
+          const currentPhrase = phrases[phraseIndex];
+          const currentLetter = currentPhrase[letterIndex];
+      
+          textEl.textContent = currentPhrase.slice(0, letterIndex);
+          cursorEl.classList.toggle("hidden");
+      
+          if (letterIndex === 0) {
+            phraseIndex = (phraseIndex + 1) % phrases.length;
+          }
+      
+          if (letterIndex > 0) {
+            letterIndex--;
+            setTimeout(deleteText, 100);
+          } else {
+            setTimeout(typeText, 1000);
+          }
+        }
+      
+        function typeText() {
+          const currentPhrase = phrases[phraseIndex];
+          const currentLetter = currentPhrase[letterIndex];
+      
+          textEl.textContent = currentPhrase.slice(0, letterIndex) + currentLetter;
+          cursorEl.classList.toggle("hidden")
+          
+          if (letterIndex === currentPhrase.length - 1) {
+            setTimeout(deleteText, 1000);
+          } else {
+            letterIndex++;
+            setTimeout(typeText, 100);
+          }
+        }
+      
+        typeText();
